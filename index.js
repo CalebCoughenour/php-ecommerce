ec2c38c10dd246faaf934312fe13247aa52c6721
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-10-31 16:27:02 -0700

    add code for carousel in top sale product section

--- index.js
+++ index.js
@@ -5,4 +5,24 @@ $(document).ready(function() {
     dots: true,
     items: 1
   });
+
+  // Product Owl Carousel
+  $("#top-sale .owl-carousel").owlCarousel({
+    loop:true,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 5
+      }
+    }
+  });
+
+
 });
