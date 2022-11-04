cdf5df3d5d497e7f5e766dca6dcf43e31e29ab5c
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-11-04 16:02:30 -0700

    add code for new phones carousel

--- index.js
+++ index.js
@@ -36,5 +36,23 @@ $(document).ready(function() {
     $grid.isotope({filter:filterValue});
   });
 
+  // New Phones Owl Carousel
+  $("#new-phones .owl-carousel").owlCarousel({
+    loop:true,
+    nav: false,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 5
+      }
+    }
+  });
+
 
 });
